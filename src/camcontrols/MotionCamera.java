ecb7cc2e234b51f20310f0df40557a860610a1ed
Dominik <Dominik@147.228.137.164>
2015-02-23 11:30:30 +0100

    Added motion camera focus and highligh variables

--- src/camcontrols/MotionCamera.java
+++ src/camcontrols/MotionCamera.java
@@ -6,8 +6,34 @@ package camcontrols;
  * @version 0.2
  */
 public class MotionCamera {
+    //Name of camera for folders
     private String name;
+    //URL to camera stream
     private String URL;
+    //if the camera stream has increased size
+    private boolean isFocused;
+    //if the camera has highlighting overlay
+    private boolean isHighlighted;
+
+    public MotionCamera(){
+        
+    }
+    
+    public boolean isIsFocused() {
+        return isFocused;
+    }
+
+    public void setIsFocused(boolean isFocused) {
+        this.isFocused = isFocused;
+    }
+    
+    public boolean isIsHighlighted() {
+        return isHighlighted;
+    }
+
+    public void setIsHighlighted(boolean isHighlighted) {
+        this.isHighlighted = isHighlighted;
+    }
 
     public String getName() {
         return name;
