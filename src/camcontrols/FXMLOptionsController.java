7794c2926108da4fbcd7276ee114d74ca3aa990f
Dominik <Dominik@Dominikovo-PC>
2015-02-25 09:14:21 +0100

    Created options window controller and basic functions created close method for options window

--- src/camcontrols/FXMLOptionsController.java
+++ src/camcontrols/FXMLOptionsController.java
@@ -7,7 +7,11 @@ package camcontrols;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -16,6 +20,37 @@ import javafx.fxml.Initializable;
  */
 public class FXMLOptionsController implements Initializable
 {
+
+    //control buttons
+
+    @FXML
+    private Button applyButton;
+
+    @FXML
+    private Button cancelButton;
+
+    //main anchor pane
+    @FXML
+    private AnchorPane mainPane;
+
+    //TODO(Dominik): Create implementation
+    @FXML
+    private void handleButtonApplyButton(final ActionEvent event)
+    {
+        System.out.println("apply button");
+    }
+
+    /**
+     * This method closes the options window
+     *
+     * @param event button clicked
+     */
+    @FXML
+    private void handleButtonCancelButton(final ActionEvent event)
+    {
+        System.out.println("Cancel button");
+        this.mainPane.getScene().getWindow().hide();
+    }
 
     /**
      * Initializes the controller class.
