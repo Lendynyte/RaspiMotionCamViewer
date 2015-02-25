f5967bbe4bf87dea7c6d92f2dbcf754d6e4750b3
Dominik <Dominik@Dominikovo-PC>
2015-02-25 09:33:49 +0100

    Cleanup

--- src/camcontrols/FXMLOptionsController.java
+++ src/camcontrols/FXMLOptionsController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package camcontrols;
 
 import java.net.URL;
@@ -16,7 +11,8 @@ import javafx.scene.layout.AnchorPane;
 /**
  * FXML Controller class
  *
- * @author Dominik
+ * @author Dominik Pauli
+ * @version  0.2
  */
 public class FXMLOptionsController implements Initializable
 {
