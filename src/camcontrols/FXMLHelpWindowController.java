1917e35e71667d5768f9a457aaa9709ef9903af9
Dominik <Dominik@Dominikovo-PC>
2015-02-25 17:25:11 +0100

    Reworked help menu now as FXML window added FXML help window and its controller

---
+++ src/camcontrols/FXMLHelpWindowController.java
@@ -0,0 +1,59 @@
+package camcontrols;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Dominik Pauli
+ * @version 0.1
+ */
+public class FXMLHelpWindowController implements Initializable
+{
+
+    //main Anchor pane
+
+    @FXML
+    private AnchorPane mainPane;
+
+    //close button
+    @FXML
+    private Button btnClose;
+
+    //Help text label
+    @FXML
+    private Label lblHelp;
+
+    //hand close operation of the button
+    @FXML
+    private void handleActionCloseHelpWindowAction(final ActionEvent event)
+    {
+        System.out.println("Cancel help");
+        this.mainPane.getScene().getWindow().hide();
+    }
+
+    /**
+     * This method sets help text in help window
+     */
+    private void setHelpText()
+    {
+        this.lblHelp.setText("Help Help\nHelp Help\nHelp Help\nHelp Help\nHelp Help\nHelp Help\nHelp Help\nHelp Help\nHelp Help\nHelp Help\n");
+    }
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        this.setHelpText();
+    }
+
+}
--- src/camcontrols/WindowMenuClass.java
+++ src/camcontrols/WindowMenuClass.java
@@ -1,17 +1,9 @@
 package camcontrols;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 /**
  *
@@ -20,53 +12,25 @@ import javafx.stage.StageStyle;
  */
 public class WindowMenuClass
 {
-    //TODO(Dominik):Maybe make this as fxml window too ?
     /**
      * This methods show basic about popup window
      *
      */
     public void createAboutWindow()
     {
-        //initialize dialog
-        Stage stage = new Stage();
-        stage.initStyle(StageStyle.UTILITY);
-
-        //Create button
-        Button btnClose = new Button("Close");
-        btnClose.setAlignment(Pos.BOTTOM_CENTER);
-
-        //hand close operation of the button
-        btnClose.setOnAction(new EventHandler<ActionEvent>()
+        try
         {
-            @Override
-            public void handle(ActionEvent event)
-            {
-                System.out.println("Closing the about window...");
-                stage.close();
-            }
-        });
-
-        //create Label with about
-        Label lbl = new Label("Help Help\nHelp Help\nHelp Help\nHelp Help\nHelp Help\nHelp Help\nHelp Help\nHelp Help\nHelp Help\nHelp Help\n");
-
-        //put vBox in stage
-        VBox vbHelp = new VBox();
-        vbHelp.setSpacing(50);
-
-        //set everything to center and make tex label expand
-        VBox.setVgrow(lbl, Priority.ALWAYS);
-        vbHelp.alignmentProperty().set(Pos.TOP_CENTER);
-
-        //add everzthing to vBox
-        vbHelp.getChildren().addAll(lbl, btnClose);
-
-        //add everything to scene
-        Scene scene = new Scene(vbHelp, 200, 250);
-
-        //show dialog
-        stage.setScene(scene);
-        stage.setTitle("Help");
-        stage.show();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("FXMLHelpWindow.fxml"));
+            Parent root = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Help");
+            stage.show();
+        } catch (Exception e)
+        {
+            System.err.println("There was an error loading help menu FXML file");
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -77,9 +41,9 @@ public class WindowMenuClass
         try
         {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("FXMLOptions.fxml"));
-            Parent root1 = (Parent) fxmlLoader.load();
+            Parent root = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
-            stage.setScene(new Scene(root1));
+            stage.setScene(new Scene(root));
             stage.setTitle("Camera options");
             stage.show();
         } catch (Exception e)
