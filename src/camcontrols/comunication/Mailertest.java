17a58e786ce1e00e8607ca8a091b5dc43ca38c8e
Dominik <Dominik@147.228.142.97>
2015-03-10 14:33:26 +0100

    added attachment sending methods

--- src/camcontrols/comunication/Mailertest.java
+++ src/camcontrols/comunication/Mailertest.java
@@ -1,18 +1,25 @@
 package camcontrols.comunication;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Date;
 import java.util.Properties;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 /**
  *
  * @author Dominik from
  * http://crunchify.com/java-mailapi-example-send-an-email-via-gmail-smtp/
+ * http://www.codejava.net/java-ee/javamail/send-e-mail-with-attachment-in-java
  */
 public class Mailertest
 {
@@ -21,9 +28,10 @@ public class Mailertest
     static Session getMailSession;
     static MimeMessage generateMailMessage;
 
-    public static void generateAndSendEmail() throws AddressException, MessagingException
+    public static void generateAndSendEmail(String imagePath) throws AddressException, MessagingException
     {
 
+//TODO(Dominik):remove messages
 //Create mail server	
         System.out.println("\n 1st ===> setup Mail Server Properties..");
         mailServerProperties = System.getProperties();
@@ -34,14 +42,40 @@ public class Mailertest
         System.out.println("Mail Server Properties have been setup successfully..");
 
 //create mail	
+        //TODO(Dominik): maybe add peg files as attachment
         System.out.println("\n\n 2nd ===> get Mail Session..");
         getMailSession = Session.getDefaultInstance(mailServerProperties, null);
         generateMailMessage = new MimeMessage(getMailSession);
         generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress("target mail"));
         //generateMailMessage.addRecipient(Message.RecipientType.CC, new InternetAddress("test2@crunchify.com"));
-        generateMailMessage.setSubject("Greetings from Crunchify..");
-        String emailBody = "Test email by Crunchify.com JavaMail API example. " + "<br><br> Regards, <br>Crunchify Admin";
-        generateMailMessage.setContent(emailBody, "text/html");
+        generateMailMessage.setSubject("Movement detected");
+        generateMailMessage.setSentDate(new Date());
+
+        String emailBody = "There was movement spotted!";
+        //generateMailMessage.setContent(emailBody, "text/html");
+
+        MimeBodyPart messageBodyPart = new MimeBodyPart();
+        messageBodyPart.setContent(emailBody, "text/html");
+
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(messageBodyPart);
+
+        MimeBodyPart attachPart = new MimeBodyPart();
+
+        //TODO(Dominik):fix
+        if (imagePath != null)
+        {
+            try
+            {
+                attachPart.attachFile(imagePath);
+            } catch (IOException ex)
+            {
+                ex.printStackTrace();
+            }
+        }
+        multipart.addBodyPart(attachPart);
+
+        generateMailMessage.setContent(multipart);
         System.out.println("Mail Session has been created successfully..");
 
 //send mail	
@@ -58,7 +92,7 @@ public class Mailertest
     public static void main(String[] args) throws MessagingException
     {
         //TODO(Dominik):you have to set application settings in google security settings
-        generateAndSendEmail();
+        generateAndSendEmail(null);
         System.out.println("\n\n ===> Your Java Program has just sent an Email successfully. Check your email..");
 
     }
